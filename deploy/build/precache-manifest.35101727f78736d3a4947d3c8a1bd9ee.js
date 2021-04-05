9768f6232fc278a2b4a3b9ad2602347790eedc75
RAFVIT <rafaelvitorfariasgoncalves@gmail.com>
2021-04-05 14:04:55 -0300

    Feat: new folder build from deploy

---
+++ deploy/build/precache-manifest.35101727f78736d3a4947d3c8a1bd9ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e6ae258d4028cd3921d623983cc6a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "109e50558f654797ea85",
+    "url": "/static/css/2.a8fa02e9.chunk.css"
+  },
+  {
+    "revision": "8a0936ec33b02c87ccdd",
+    "url": "/static/css/main.05d2b53b.chunk.css"
+  },
+  {
+    "revision": "109e50558f654797ea85",
+    "url": "/static/js/2.b4396bd1.chunk.js"
+  },
+  {
+    "revision": "13b2deb8eae3936cfa8e6363673b4f98",
+    "url": "/static/js/2.b4396bd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a0936ec33b02c87ccdd",
+    "url": "/static/js/main.542208c8.chunk.js"
+  },
+  {
+    "revision": "fc537a9bac466c9b1d54",
+    "url": "/static/js/runtime-main.94830510.js"
+  },
+  {
+    "revision": "85c12dadde38c2f94252babc154823d2",
+    "url": "/static/media/mainlogo.85c12dad.png"
+  }
+]);
