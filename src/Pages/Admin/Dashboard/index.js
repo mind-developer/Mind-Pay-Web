879e5dc183259a96958998c015653f83a08fd07b
RAFVIT <rafaelvitorfariasgoncalves@gmail.com>
2021-04-05 08:06:17 -0300

    Feat tabela de pagamentos

--- src/Pages/Admin/Dashboard/index.js
+++ src/Pages/Admin/Dashboard/index.js
@@ -1,11 +1,14 @@
+/* eslint-disable no-useless-concat */
+/* eslint-disable no-unused-expressions */
 import React, { useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import useSWR from "swr";
 import { AuthContext } from '../../../providers/auth';
 import Axios from 'axios';
-import { Col, Row, Spin, Avatar } from 'antd';
-import { VictoryChart, VictoryLine, VictoryScatter } from "victory";
+import { Col, Row, Spin, Avatar, Table } from 'antd';
+import { VictoryChart, VictoryLine } from "victory";
 import { LoadingOutlined } from '@ant-design/icons';
+import { FormatValor } from '../../../utils/format'
 
 
 const Container = styled.div`
@@ -15,7 +18,6 @@ const Container = styled.div`
     align-items: center;
     height: 100%;
     text-align: center;
-
     
     h1{
         font-size: 42px;
@@ -54,23 +56,64 @@ const chartTheme = {
   },
 };
 
-const months = ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez'];
+const months = ['JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN', 'JUL', 'AGO', 'SET', 'OUT', 'NOV', 'DEZ'];
 
 function Dashboard() {
 
   const { fetcher } = useContext(AuthContext);
 
   const { data } = useSWR('/users/count', fetcher);
-  const { data: payments } = useSWR('/users/payments', fetcher);
 
   const [pagamentos, setPagamentos] = useState(null)
-  const [count, setCount] = useState(0)
 
   const [minder, setMinder] = useState(null)
 
   const { data: users } = useSWR('/users', fetcher)
 
-  const [ano, setAno] = useState(new Date().getYear())
+  const [ano, setAno] = useState(new Date().getFullYear())
+
+  const [valores, setValores] = useState([])
+
+  const columns = [
+    {
+      title: 'Meses',
+      dataIndex: 'mes',
+      key: 'mes',
+    },
+    {
+      title: 'Salario base',
+      dataIndex: 'base',
+      key: 'base',
+    },
+    {
+      title: 'Hora extra',
+      dataIndex: 'extra',
+      key: 'extra',
+    },
+    {
+      title: 'Total',
+      key: 'total',
+      dataIndex: 'total',
+    },
+  ];
+
+  useEffect(() => {
+
+    if (pagamentos !== []) {
+      const aux = []
+      pagamentos?.amounted?.forEach((total, index) => {
+        aux.push({
+          base: FormatValor(pagamentos.base[index]),
+          mes: months[index],
+          extra: FormatValor(pagamentos.extra[index]),
+          total: FormatValor(total)
+        });
+        return aux;
+      })
+      setValores(aux);
+    }
+
+  }, [pagamentos])
 
   async function loading() {
     Axios.get('/users/payments', {
@@ -79,7 +122,6 @@ function Dashboard() {
 
       },
     }).then(res => {
-      // console.log(res.data);
 
       setPagamentos(res.data)
     }).catch(err => {
@@ -87,7 +129,6 @@ function Dashboard() {
     })
   }
   async function filterName() {
-
     if (minder) {
       const resp = await Axios.post('/users/filter', {
         minder: minder,
@@ -107,7 +148,6 @@ function Dashboard() {
 
   }, []);
 
-
   if (!data && !pagamentos) {
     return <Spin indicator={<LoadingOutlined />} />
   }
@@ -122,29 +162,34 @@ function Dashboard() {
           <h1 style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginBottom: 0, marginRight: 20 }}>
             Escolha um minder e o ano:
         </h1>
-          <select id="minder" style={{ height: 35, width: 130, borderRadius: 10 }} onChange={(e) => setMinder(e?.target?.value)}>
-            <option value="0">Todos</option>
+          <select id="minder" style={{ height: 35, width: 130, borderRadius: 5, borderColor: '#ccc' }} onChange={(e) => setMinder(e?.target?.value)}>
+            <option value="0" style={{ marginLeft: 10 }}>
+              Todos
+            </option>
             {users?.data?.map((user) =>
               <option value={user?.id}>{user?.name}</option>
             )}
 
           </select>
 
-          <select id="ano" style={{ height: 35, width: 130, marginLeft: 10, borderRadius: 10 }} onChange={(e) => setAno(e?.target?.value)}>
-            <option value="2016">2016</option>
-            <option value="2017">2017</option>
-            <option value="2018">2018</option>
-            <option value="2019">2019</option>
-            <option value="2020">2020</option>
+          <select id="ano" style={{ height: 35, width: 130, marginLeft: 10, borderRadius: 5, borderColor: '#ccc' }} onChange={(e) => setAno(e?.target?.value)}>
             <option value="2021">2021</option>
-
+            <option value="2022">2022</option>
+            <option value="2023">2023</option>
+            <option value="2024">2024</option>
+            <option value="2025">2025</option>
+            <option value="2026">2026</option>
+            <option value="2027">2027</option>
+            <option value="2028">2028</option>
+            <option value="2029">2029</option>
+            <option value="2030">2030</option>
           </select>
           <button
             style={{
               height: 35,
               width: 130,
               marginLeft: 10,
-              borderRadius: 10,
+              borderRadius: 5,
               backgroundColor: '#7B2627',
               color: '#fff',
               elevation: 15,
@@ -154,7 +199,7 @@ function Dashboard() {
           >Procurar</button>
         </div>
         {
-          pagamentos && (
+          pagamentos != [] ? (
             <VictoryChart height={250} theme={chartTheme}>
               <VictoryLine
                 interpolation={'linear'} data={pagamentos?.amounted?.map((i, index) => { return { x: months[index], y: i } })}
@@ -169,7 +214,7 @@ function Dashboard() {
                 style={{ data: { stroke: "#00008b" } }}
               />
             </VictoryChart>
-          ) 
+          ) : <p>Não Há dados</p>
         }
       </Col>
 
@@ -196,14 +241,18 @@ function Dashboard() {
       </div>
 
       <Col span={24} style={{ marginBottom: 50 }}>
+        <div style={{ width: '100%', marginTop: 25 }}>
+          <Table columns={columns} dataSource={valores} pagination={{ pageSize: 12 }} />
+        </div>
         <h1>
-          já somos {data?.data.length} Minders
+          Já somos {data?.data.length} Minders
         </h1>
         {data?.data.map(item => (
           <Avatar alt={item.name} size={50} style={{ objectFit: 'cover' }} src={Axios.defaults.baseURL + "media/" + item.profile_image}>{item.name[0]}</Avatar>
         ))}
       </Col>
     </Row>
+
   </Container>;
 }
 
---
+++ src/utils/format.js
@@ -0,0 +1,4 @@
+export const { format: FormatValor } = new Intl.NumberFormat('pt-Br', {
+    style: 'currency',
+    currency: 'BRL'
+})
